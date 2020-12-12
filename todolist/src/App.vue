324765c78ad379b85b6a6f21089217eb3648ac7b
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2020-12-12 18:08:19 +0000

    Update App.vue

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <Header />
-  <AddTodo v-on:add-todo="addTodo"/>
-  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <style>
